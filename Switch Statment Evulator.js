da41e4ac0d5e6555d5d41d17adf7df7e1df2af65
Mundax <mundalamo@protonmail.com>
2024-11-04 18:54:44 +0200

    Add files via upload

---
+++ Switch Statment Evulator.js
@@ -0,0 +1,22 @@
+// Creating a friend name checker
+
+let userName = prompt("What is your name?");
+
+switch(userName) {
+    case "Lufuno":
+        console.log("Yes, " 
+            + userName + ", Your name is on our database");
+        break;
+    case "Rotondwa":
+        console.log("Yes, " + userName + ", Your name is on our database");
+        break;
+    case "Matamela":
+        console.log("Yes, " + userName + ", Your name is on our database");
+        break;
+    case "James":
+        console.log("Yes, " + userName + ", Your name is on our database");
+        break;
+    default:
+        console.log("We can not find your name here");
+        break;
+}
